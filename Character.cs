523d6fef067acf621d7d93593b13d7070bbf6ee8
superjoy0502 <superjoy0502@gmail.com>
2020-06-10 22:21:23 -0600

    Initial commit

---
+++ Character.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+
+    public string jsonname;
+
+    public string path;
+    public DialogueElements dialogues;
+
+    private void Awake()
+    {
+        path = Directory.GetCurrentDirectory() + "\\Assets\\Local\\" + jsonname + ".json";
+    }
+
+    private void Start()
+    {
+        dialogues = new DialogueElements();
+        Debug.Log(File.ReadAllText(path));
+        dialogues = (DialogueElements) JsonUtility.FromJson<DialogueElements>("{\"elements\":" + File.ReadAllText(path) + "}");
+
+        #region Convert2Json (Test)
+        // string[] hi = {"Hello", "World!"};
+        // string[] idlehi = {"Hello", "Idle!"};
+        //
+        // dialogue.name = "John";
+        // dialogue.txt = hi;
+        // dialogue.idletxt = idlehi;
+        //
+        // Debug.Log(JsonUtility.ToJson(dialogue));
+        #endregion
+
+        Debug.Log(dialogues.elements[1].txt[0]);
+    }
+}
---
+++ DialogueElement.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+[Serializable]
+public class DialogueElement
+{
+    public string name; // 캐릭터 이름
+    public string[] txt; // 캐릭터 스토리 대사
+    public string[] idletxt; // 캐릭터 노말 대사
+}
+
+[Serializable]
+public class DialogueElements
+{
+    public DialogueElement[] elements;
+}
